45687b844d8474185587dbc874ee8187d36eacf7
jashvirr <jashr212@gmail.com>
2024-09-09 18:18:30 -0400

    added Multiples and completed main that returns mutliples

---
+++ src/Multiples.java
@@ -0,0 +1,17 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+        int totalMultiples = 0;
+
+        for (int i = 1; i < 1000; i++) {
+
+            if (i % 3 == 0 || i % 5 == 0) {
+                totalMultiples++;
+            }
+
+        }
+
+        System.out.println(totalMultiples);
+    }
+
+}
