1bf0f1c0d612f8b8159257188872fef78551f988
jashvirr <jashr212@gmail.com>
2024-09-09 18:07:33 -0400

    updated main.main with fizzbuzzmain call

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-        FizzBuzz.Fizzer(3);
+        String[] args2 = new String[]{"hello"};
+        FizzBuzz.main(args2);
     }
 }
